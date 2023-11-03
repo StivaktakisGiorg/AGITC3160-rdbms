c0d507d50335e91704eb43ed4d543f985689fc81
avamvakas02 <149789816+avamvakas02@users.noreply.github.com>
2023-11-03 15:01:33 +0200

    Add files via upload

---
+++ Q1.sql
@@ -0,0 +1,2 @@
+--QUSTION 1
+select w.firstname,w.lastname from worker w, dept d where salary>30000 and departmentname='Development';
---
+++ Q3.sql
@@ -0,0 +1,2 @@
+Question 3
+select projno, projname, budget/12 from project;
